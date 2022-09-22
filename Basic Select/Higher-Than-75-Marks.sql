9149194cc2aa1205bac06b7bdee72c0f1b6b925f
nottherightlynx <114174670+nottherightlynx@users.noreply.github.com>
2022-09-22 11:17:42 -0600

    Update Higher-Than-75-Marks.sql

--- Basic Select/Higher-Than-75-Marks.sql
+++ Basic Select/Higher-Than-75-Marks.sql
@@ -1,2 +1,11 @@
 # Author: Thomas George Thomas
 select name from students where marks > 75 order by right(name,3),id asc;
+
+# another option to use substr() func
+
+select name
+from students
+where
+    marks > 75
+ORDER BY
+    SUBSTR(name, length(name)-2,3), ID;
